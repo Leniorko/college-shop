e271950edd89d706751d0f8f8a0bcd2383cbe696
Ivan Egorov <www.Len244@gmail.com>
2022-03-10 00:08:08 +0300

    Added bas migrations

--- database/migrations/2022_03_09_195711_create_users_table.php
+++ database/migrations/2022_03_09_195711_create_users_table.php
@@ -8,18 +8,20 @@ class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * Generates user table
      * @return void
      */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("name");
+            $table->string("surname");
+            $table->string("patronymic")->nullable();
+            $table->string("login");
+            $table->string("email")->unique();
+            $table->string("password");
+            $table->boolean("rules");
             $table->timestamps();
         });
     }
---
+++ database/migrations/2022_03_09_203830_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * Generates products table
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->float("price");
+            $table->text("img_url");
+            $table->foreignId('category_id')->constrained();
+            // Extra data that not critical to the system.
+            $table->string('country')->nullable(); // Can be placed to it's own table
+            $table->integer('creation_year', false, true)->nullable();
+            $table->string('model')->nullable(); // Can be placed to it's own table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
