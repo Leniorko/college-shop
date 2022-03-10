d030e5a88a5575ef24591b44526060f88a3cf489
Ivan Egorov <www.Len244@gmail.com>
2022-03-10 09:57:42 +0300

    Updated models with fillable fields

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'img_url',
+        'category_id',
+        'country',
+        'creation_year',
+        'model',
+    ];
 }
